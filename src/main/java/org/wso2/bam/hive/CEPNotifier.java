16ea6fe409b72a9f033d5b90abb6392329f90470
zamly <zamly@wso2.com>
2014-12-17 14:57:13 +0530

    Updated improvements

--- src/main/java/org/wso2/bam/hive/CEPNotifier.java
+++ src/main/java/org/wso2/bam/hive/CEPNotifier.java
@@ -1,3 +1,20 @@
+/*
+*  Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 package org.wso2.bam.hive;
 
 import org.wso2.carbon.analytics.hive.extension.AnalyzerContext;
@@ -9,42 +26,106 @@ import org.wso2.carbon.databridge.commons.exception.*;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.util.Date;
 
-/**
- * Created by zamly-PC on 12/11/14.
- */
+import org.apache.log4j.Logger;
+
+
 public class CEPNotifier implements HiveAnalyzer {
 
+    /** Stream name */
     public static String STREAM_NAME = "";
+
+    /** Stream version */
     public static String VERSION = "";
-    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(CEPNotifier.class);
+
+    /** Host address of CEP */
+    public static String cepHost = "localhost";
+
+    /** Port of CEP */
+    public static String cepPort = "7611";
+
+    public static String username = "admin";
+    public static String password = "admin";
+
+    private static Logger logger = Logger.getLogger(CEPNotifier.class);
 
     @Override
     public void execute(AnalyzerContext analyzerContext) {
 
         String timestampNow = analyzerContext.getProperty("TIMESTAMP_SCRIPT_STARTED");
-        String timestamp = analyzerContext.getParameters().get("timestamp");
+        //String timestamp = analyzerContext.getParameters().get("timestamp");
 
-        if (!analyzerContext.getParameters().get("stream").equals(null)) {
+        // checks if the stream parameter is null
+        if (!analyzerContext.getParameters().get("stream").equals("")) {
             STREAM_NAME = analyzerContext.getParameters().get("stream");
+        } else {
+            logger.error("Parameter stream required.");
         }
 
-        if (!analyzerContext.getParameters().get("version").equals(null)) {
+        // checks if the version parameter is null
+        if (!analyzerContext.getParameters().get("version").equals("")) {
            VERSION = analyzerContext.getParameters().get("version");
+        } else {
+            logger.error("Parameter version required.");
         }
 
-        //logger.info("testing......."+ timestampNow);
-        long nowTimestamp = Long.parseLong(timestampNow);
+        if (!analyzerContext.getParameters().get("cepHost").equals("")) {
+            cepHost = analyzerContext.getParameters().get("cepHost");
+        } else {
+            logger.error("Parameter cepHost required.");
+        }
+
+        if (!analyzerContext.getParameters().get("cepPort").equals("")) {
+            cepPort = analyzerContext.getParameters().get("cepPort");
+        } else {
+            logger.error("Parameter cepPort required.");
+        }
+
+        if (!analyzerContext.getParameters().get("username").equals("")) {
+            username = analyzerContext.getParameters().get("username");
+        } else {
+            logger.error("Parameter username required.");
+        }
+
+        if (!analyzerContext.getParameters().get("password").equals("")) {
+            password = analyzerContext.getParameters().get("password");
+        } else {
+            logger.error("Parameter password required.");
+        }
 
         try {
-            testSendingEvent(nowTimestamp);
+            sendEvent(Long.parseLong(timestampNow));
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Event Sending Error: "+ e.getMessage());
         }
     }
 
-    public void testSendingEvent(long timestampNow)
+
+    /**
+     * Method sends the timestamp as an event to
+     * the specified thrift endpoint.
+     *
+     * @param timestampNow
+     *            current time stamp
+     * @throws MalformedURLException
+     *             the malformed url exception
+     * @throws AgentException
+     *             the agent exception
+     * @throws AuthenticationException
+     *             the authentication exception
+     * @throws TransportException
+     *             the transport exception
+     * @throws MalformedStreamDefinitionException
+     *             the malformed stream definition exception
+     * @throws StreamDefinitionException
+     *             the stream definition exception
+     * @throws DifferentStreamDefinitionAlreadyDefinedException
+     *             the different stream definition already defined exception
+     * @throws InterruptedException
+     *             the interrupted exception
+     * @throws UndefinedEventTypeException
+     */
+    public void sendEvent(long timestampNow)
             throws MalformedURLException, AuthenticationException, TransportException,
             AgentException, UndefinedEventTypeException,
             DifferentStreamDefinitionAlreadyDefinedException,
@@ -54,12 +135,12 @@ public class CEPNotifier implements HiveAnalyzer {
 
         setTrustStoreParams();
 
-        Agent agent = new Agent();
-        DataPublisher dataPublisherCEP = new DataPublisher("tcp://localhost:7611", "admin", "admin", agent);
+        //Agent agent = new Agent();
+        DataPublisher dataPublisherCEP = new DataPublisher("tcp://"+cepHost+":"+cepPort, username, password);
 
         String streamId = getStreamID(dataPublisherCEP);
 
-        logger.info("1st stream defined: "+streamId);
+        logger.info("Stream defined: "+streamId);
 
         dataPublisherCEP.publish(streamId, new Object[]{"127.0.0.1"}, new Object[]{Integer.toString(1)},
                 new Object[]{timestampNow});
@@ -92,28 +173,23 @@ public class CEPNotifier implements HiveAnalyzer {
 
         String streamId = null;
 
-        try {
-            streamId = dataPublisher.findStreamId(STREAM_NAME, VERSION);
-        } catch (Exception e) {
-
-            streamId =
-                    dataPublisher.defineStream("{" +
-                            "  'name':'" + STREAM_NAME + "'," +
-                            "  'version':'" + VERSION + "'," +
-                            "  'nickName': 'Pressure Sensing Information'," +
-                            "  'description': 'Some Desc'," +
-                            "  'tags':['foo', 'bar']," +
-                            "  'metaData':[" +
-                            "          {'name':'ipAdd','type':'STRING'}" +
-                            "  ]," +
-                            "  'correlationData':[" +
-                            "          {'name':'correlationId','type':'STRING'}" +
-                            "  ]," +
-                            "  'payloadData':[" +
-                            "          {'name':'timestamp','type':'LONG'}" +
-                            "  ]" +
-                            "}");
-        }
+        streamId =
+                dataPublisher.defineStream("{" +
+                        "  'name':'" + STREAM_NAME + "'," +
+                        "  'version':'" + VERSION + "'," +
+                        "  'nickName': 'Pressure Sensing Information'," +
+                        "  'description': 'Some Desc'," +
+                        "  'tags':['foo', 'bar']," +
+                        "  'metaData':[" +
+                        "          {'name':'ipAdd','type':'STRING'}" +
+                        "  ]," +
+                        "  'correlationData':[" +
+                        "          {'name':'correlationId','type':'STRING'}" +
+                        "  ]," +
+                        "  'payloadData':[" +
+                        "          {'name':'timestamp','type':'LONG'}" +
+                        "  ]" +
+                        "}");
 
         return streamId;
     }
--- src/main/java/org/wso2/bam/hive/SetScheduleStartTime.java
+++ src/main/java/org/wso2/bam/hive/SetScheduleStartTime.java
@@ -1,20 +1,34 @@
+/*
+*  Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 package org.wso2.bam.hive;
 
 import org.wso2.carbon.analytics.hive.extension.AnalyzerContext;
 import org.wso2.carbon.analytics.hive.extension.HiveAnalyzer;
 
-import java.util.Date;
 
-/**
- * Created by zamly-PC on 12/11/14.
- */
 public class SetScheduleStartTime implements HiveAnalyzer {
     @Override
     public void execute(AnalyzerContext analyzerContext) {
 
-        Date now = new Date();
-        long nowTimeStamp = now.getTime();
+        //current time in millis
+        long nowTimeStamp = System.currentTimeMillis();
 
+        //current timestamp is set as a property
         analyzerContext.setProperty("TIMESTAMP_SCRIPT_STARTED", String.valueOf(nowTimeStamp));
     }
 }
